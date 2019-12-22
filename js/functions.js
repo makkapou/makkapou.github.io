7efe317f558e2504d56934f207e70c9f23d71b9a
lee-unext <l-chen@unext.jp>
2019-12-22 16:57:25 +0900

    modify text

--- js/functions.js
+++ js/functions.js
@@ -116,6 +116,7 @@ function startHeartAnimation() {
 function timeElapse(date) {
   var current = Date();
   var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+  console.log(seconds);
   var days = Math.floor(seconds / (3600 * 24));
   seconds = seconds % (3600 * 24);
   var hours = Math.floor(seconds / 3600);
