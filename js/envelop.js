a8ce491b4f120d6d3e6d2e96bcc32b228a487237
lee-unext <l-chen@unext.jp>
2020-01-25 02:57:46 +0900

    add envelop

---
+++ js/envelop.js
@@ -0,0 +1,7 @@
+var oChai = document.getElementById('chai');
+var oClose = document.getElementById('close');
+var oContainer = document.getElementById('red-envelop');
+
+oChai.addEventListener('click', function () {
+  oChai.classList.add('rotate');
+});
