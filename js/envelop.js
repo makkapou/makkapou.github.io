b04c44ba0e01e3d2ff2172620a3eff2c558a18e0
lee-unext <l-chen@unext.jp>
2020-03-02 00:21:46 +0900

    delete envelop

--- js/envelop.js
+++
@@ -1,7 +0,0 @@
-var oChai = document.getElementById('chai');
-var oClose = document.getElementById('close');
-var oContainer = document.getElementById('red-envelop');
-
-oChai.addEventListener('click', function () {
-  oChai.classList.add('rotate');
-});
